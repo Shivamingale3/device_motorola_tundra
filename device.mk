998b2e3bd3cc8eb3f0ba941215a6ba1233f219d7
Shivamingale3 <shivamingale3@gmail.com>
2024-11-18 11:11:43 +0530

    Remove offending entries: android.hardware.thermal@2.0-service.qti android.hardware.usb@1.3-service-qti lowi.conf
    Signed-off-by: Shivamingale3 <shivamingale3@gmail.com>

--- device.mk
+++ device.mk
@@ -217,7 +217,7 @@ PRODUCT_PACKAGES += \
     gnss_antenna_info.conf \
     gps.conf \
     izat.conf \
-    lowi.conf \
+#    lowi.conf \
     sap.conf
 
 PRODUCT_PACKAGES += \
@@ -467,8 +467,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+#PRODUCT_PACKAGES += \
+#    android.hardware.thermal@2.0-service.qti
 
 # Ueventd
 PRODUCT_COPY_FILES += \
@@ -484,8 +484,8 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
 # USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+#PRODUCT_PACKAGES += \
+#    android.hardware.usb@1.3-service-qti
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
